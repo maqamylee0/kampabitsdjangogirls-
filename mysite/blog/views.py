f397f7d59dc1b7d1e96b757a2d9c956304125521
maqamylee0 <maqamylee0@gmail.com>
2021-05-04 05:18:25 +0300

    initialcommit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
+
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello welcome to our home")
+    return render(request,"index.html")
 
 def about(request):
-    return HttpResponse("we serve")    
+    return render(request,"about.html")    
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
